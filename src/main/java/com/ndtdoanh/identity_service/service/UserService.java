9a265bd869f73f17c3f88c5c2904566d9042d204
doanh <nguyentrongdoanh202@gmail.com>
2024-11-22 18:43:03 +0700

    API crud

---
+++ src/main/java/com/ndtdoanh/identity_service/service/UserService.java
@@ -0,0 +1,53 @@
+package com.ndtdoanh.identity_service.service;
+
+import com.ndtdoanh.identity_service.dto.request.UserRequest;
+import com.ndtdoanh.identity_service.dto.request.UserUpdateRequest;
+import com.ndtdoanh.identity_service.entity.User;
+import com.ndtdoanh.identity_service.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(UserRequest request){
+        User user = new User();
+
+        user.setUsername(request.getUsername());
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setDob(request.getDob());
+
+        return userRepository.save(user);
+    }
+
+    public List<User> getUsers(){
+        return userRepository.findAll();
+    }
+
+    public User getUser(String id){
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    public User updateUser(String userId, UserUpdateRequest request){
+        User user = getUser(userId);
+
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setDob(request.getDob());
+
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(String id){
+        userRepository.deleteById(id);
+    }
+
+}
