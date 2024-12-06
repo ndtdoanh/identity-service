1490c31614d263a3ef3853edf028c87bf9bac4f2
doanh <nguyentrongdoanh202@gmail.com>
2024-12-06 19:18:14 +0700

    profile & environment variables

--- src/main/java/com/ndtdoanh/identity_service/service/AuthenticationService.java
+++ src/main/java/com/ndtdoanh/identity_service/service/AuthenticationService.java
@@ -76,6 +76,7 @@ public class AuthenticationService {
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        log.info("SignKey: {}", SIGNER_KEY);
         PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10);
         var user = userRepository
                 .findByUsername(request.getUsername())
