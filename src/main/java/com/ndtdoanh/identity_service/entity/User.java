093e1af09fd85d7b6ac755171721ffbd7b017a7f
doanh <nguyentrongdoanh202@gmail.com>
2024-12-06 18:32:06 +0700

    unique field, concurrent request

--- src/main/java/com/ndtdoanh/identity_service/entity/User.java
+++ src/main/java/com/ndtdoanh/identity_service/entity/User.java
@@ -3,6 +3,7 @@ package com.ndtdoanh.identity_service.entity;
 import java.time.LocalDate;
 import java.util.Set;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -29,6 +30,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.UUID)
     String id;
 
+    @Column(name = "username", unique = true, columnDefinition = "VARCHAR(255) COLLATE utf8mb4_unicode_ci")
     String username;
     String password;
     String firstName;
--- src/main/java/com/ndtdoanh/identity_service/service/UserService.java
+++ src/main/java/com/ndtdoanh/identity_service/service/UserService.java
@@ -3,6 +3,8 @@ package com.ndtdoanh.identity_service.service;
 import java.util.HashSet;
 import java.util.List;
 
+import com.ndtdoanh.identity_service.constant.PredefinedRole;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -13,7 +15,7 @@ import com.ndtdoanh.identity_service.dto.request.UserRequest;
 import com.ndtdoanh.identity_service.dto.request.UserUpdateRequest;
 import com.ndtdoanh.identity_service.dto.response.UserResponse;
 import com.ndtdoanh.identity_service.entity.User;
-import com.ndtdoanh.identity_service.enums.Role;
+import com.ndtdoanh.identity_service.entity.Role;
 import com.ndtdoanh.identity_service.exception.AppException;
 import com.ndtdoanh.identity_service.exception.ErrorCode;
 import com.ndtdoanh.identity_service.mapper.UserMapper;
@@ -36,19 +38,22 @@ public class UserService {
     PasswordEncoder passwordEncoder;
 
     public UserResponse createUser(UserRequest request) {
-        log.info("Service: create User");
-
-        if (userRepository.existsByUsername(request.getUsername())) throw new AppException(ErrorCode.USER_EXISTED);
 
         User user = userMapper.toUser(request);
         user.setPassword(passwordEncoder.encode(user.getPassword()));
 
-        HashSet<String> roles = new HashSet<>();
-        roles.add(Role.USER.name());
+        HashSet<Role> roles = new HashSet<>();
+        roleRepository.findById(PredefinedRole.USER_ROLE).ifPresent(roles::add);
 
-        //        user.setRoles(roles);
+        user.setRoles(roles);
 
-        return userMapper.toUserResponse(userRepository.save(user));
+        try {
+            user = userRepository.save(user);
+        } catch (DataIntegrityViolationException exception){
+            throw new AppException(ErrorCode.USER_EXISTED);
+        }
+
+        return userMapper.toUserResponse(user);
     }
 
     public UserResponse getMyInfo() {
